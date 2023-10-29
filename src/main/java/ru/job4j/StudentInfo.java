cf475b13351dbb10cb88089f34ad5a997f805762
AleksandrVasyunin <skil@inbox.ru>
2023-10-29 22:46:14 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Hello, Aleks!");
+        System.out.println("25.10.2023 - start java");
+    }
+}
